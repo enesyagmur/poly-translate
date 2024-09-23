1f22b6d6648bc9b30c0d90892806dfd5c360bbb8
erain <enesyagmur99@gmail.com>
2024-09-23 16:58:24 +0300

    api converter fonksiyonuna koşul eklendi ve bazı ufak değişiklikler

--- src/Api/converter.js
+++ src/Api/converter.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const converterFunc = async (inputText, sourceLang, targetLang) => {
   try {
     const response = await axios.get(
-      `https://api.mymemory.translated.net/get`,
+      "https://api.mymemory.translated.net/get",
       {
         params: {
           q: inputText,
@@ -11,11 +11,14 @@ const converterFunc = async (inputText, sourceLang, targetLang) => {
         },
       }
     );
-
-    return response.data.responseData.translatedText;
+    if (response.data && response.data.responseData) {
+      return response.data.responseData.translatedText;
+    } else {
+      throw new Error("Invalid response format");
+    }
   } catch (error) {
-    console.error("Api error:", error);
-    throw error;
+    console.error("API error:", error.message);
+    throw new Error("Translation failed");
   }
 };
 
